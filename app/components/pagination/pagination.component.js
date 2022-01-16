123d3381e8f0400f8aba7c468ae8ec2cc33c3ce1
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2022-01-16 16:28:21 -0300

    Ajuste paginação

--- app/components/pagination/pagination.component.js
+++ app/components/pagination/pagination.component.js
@@ -11,7 +11,7 @@ appMarvel.directive('pagination', function () {
 		},
 		controller: function ($scope) {
 			$scope.pages = Array.from({length: Math.ceil($scope.total / $scope.items)}, (v, k) => (k + 1));
-			$scope.size = 5;
+			$scope.size = 4;
 			$scope.startPage = ($scope.page - $scope.size) <= 0 ? 1 : (($scope.page - $scope.size) + 1);
 		},
 		templateUrl: '/app/components/pagination/pagination.component.html',
